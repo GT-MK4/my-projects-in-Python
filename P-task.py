ed5d3d1bec986f0ccb76a0d74b0a1f06e648e889
GT Moukim <lamraouimoukim@gmail.com>
2026-01-08 00:55:23 +0100

    Update P-task.py

--- P-task.py
+++ P-task.py
@@ -1,42 +1,26 @@
 print("---welcome to GT task manager---")
-task1=""
-task2=""
-task3=""
-task4=""
-task5=""
-task=[task1,task2,task3,task4,task5]
-print("---welcome to GT task manager---")
+tasks=[]
 i= 0
 while True:
-    num=int(input("choose ur number (1-4):\n 1.add task\n 2.view tasks\n 3.delete task\n 4.exit \n enter ur choice: "))
+    num=int(input("\n choose ur number (1-4):\n 1.add task\n 2.view tasks\n 3.delete task\n 4.exit \n enter ur choice: "))
     print("1.add task\n 2.view tasks \n 3.delete task \n 4.exit")
     if num==1:
-        task_name = input("enter task name: ")
-        if task1=="":
-            task1=task_name
-            print("task added\n")
-        elif task2=="" and task1!="":
-            task2=task_name
-            print("task added\n")
-        elif task3=="" and task2!="":
-            task3=task_name
-            print("task added\n")
-        elif task4=="" and task3!="":
-            task4=task_name
-            print("task added\n")
+        new_task = input("enter task name: ")
+        tasks.append(new_task)
     print("\n")
     if num==2:
         print("---your tasks are:")
-        for t in task:
+        for t in tasks:
             if t!="":
                 i+=1
                 print(f"{i}. {t}")
-                print("complet delete!!")
+                print("\n this all tasks")
     if num==3:
         num=int(input("enter task number to delete: "))
-        print("task")
+        print(f"{i}. {t}")
         num+=1
-        task.pop(num-1)
+        tasks.pop(num-1)
+        print("complet delete !!")
     if num==4:
         print("exiting task manager")
         break
